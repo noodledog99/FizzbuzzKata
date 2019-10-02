446653ee1f609e753417b555ee394a3103ad1137
lonelycay99 <nerddy.hahaha@hotmail.com>
2019-10-02 15:48:04 +0700

    create console, xunit and reference project

---
+++ FizzbuzzConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FizzbuzzConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
