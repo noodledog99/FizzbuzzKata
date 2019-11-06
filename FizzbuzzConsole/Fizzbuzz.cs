82f156f9ee557e7f755dc0aaf82ee7c703b6dec0
lonelycay99 <nerddy.hahaha@hotmail.com>
2019-11-06 20:20:25 +0700

    Input 3 should return Fizz

--- FizzbuzzConsole/Fizzbuzz.cs
+++ FizzbuzzConsole/Fizzbuzz.cs
@@ -6,6 +6,14 @@ namespace FizzbuzzController
     {
         public string Say(int number)
         {
+            if(number == 3)
+            {
+                return "Fizz";
+            }
+            if(number == 2)
+            {
+                return "2";
+            }
             return "1";
         }
     }
--- FizzbuzzTest/FizzbuzzTDD.cs
+++ FizzbuzzTest/FizzbuzzTDD.cs
@@ -14,5 +14,21 @@ namespace FizzbuzzTest
             var actual = fizzbuzz.Say(1);
             Assert.Equal("1",actual);
         }
+
+        [Fact]
+        public void WhenInput2ShouldSay2()
+        {
+            var fizzbuzz = new Fizzbuzz();
+            var actual = fizzbuzz.Say(2);
+            Assert.Equal("2",actual);
+        }
+
+        [Fact]
+        public void WhenInput3ShouldSayFizz()
+        {
+            var fizzbuzz = new Fizzbuzz();
+            var actual = fizzbuzz.Say(3);
+            Assert.Equal("Fizz",actual);
+        }
     }
 }
