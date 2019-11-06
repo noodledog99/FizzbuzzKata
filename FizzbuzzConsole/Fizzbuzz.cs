81d7ef4f449e64dfed474aa90f31c092d17f674b
lonelycay99 <nerddy.hahaha@hotmail.com>
2019-11-06 20:34:25 +0700

    Input 5 should return buzz

--- FizzbuzzConsole/Fizzbuzz.cs
+++ FizzbuzzConsole/Fizzbuzz.cs
@@ -6,6 +6,10 @@ namespace FizzbuzzController
     {
         public string Say(int number)
         {
+            if(number == 5)
+            {
+                return "Buzz";
+            }
             if(number == 3)
             {
                 return "Fizz";
--- FizzbuzzTest/FizzbuzzTDD.cs
+++ FizzbuzzTest/FizzbuzzTDD.cs
@@ -15,28 +15,35 @@ namespace FizzbuzzTest
         public void WhenInput1ShouldSay1()
         {
             var actual = fizzbuzz.Say(1);
-            Assert.Equal("1",actual);
+            Assert.Equal("1", actual);
         }
 
         [Fact]
         public void WhenInput2ShouldSay2()
         {
             var actual = fizzbuzz.Say(2);
-            Assert.Equal("2",actual);
+            Assert.Equal("2", actual);
         }
 
         [Fact]
         public void WhenInput3ShouldSayFizz()
         {
             var actual = fizzbuzz.Say(3);
-            Assert.Equal("Fizz",actual);
+            Assert.Equal("Fizz", actual);
         }
 
         [Fact]
         public void WhenInput4ShouldSay4()
         {
             var actual = fizzbuzz.Say(4);
-            Assert.Equal("4",actual);
+            Assert.Equal("4", actual);
+        }
+
+        [Fact]
+        public void WhenInput5ShouldSayBuzz()
+        {
+            var actual = fizzbuzz.Say(5);
+            Assert.Equal("Buzz", actual);
         }
     }
 }
