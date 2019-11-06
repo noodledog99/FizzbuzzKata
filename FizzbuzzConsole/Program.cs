144647ee6ec6559a02ab720856a37b2bd1c1c1b0
lonelycay99 <nerddy.hahaha@hotmail.com>
2019-11-06 21:40:06 +0700

    Implement fizzbuzz in console

--- FizzbuzzConsole/Program.cs
+++ FizzbuzzConsole/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using FizzbuzzController;
 
 namespace FizzbuzzConsole
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+            var filePath = "./number.txt";
+            if(!File.Exists(filePath))
+            {
+                using(StreamWriter file = new StreamWriter(filePath))
+                {
+                    foreach (var number in Enumerable.Range(1,100))
+                    {
+                        file.WriteLine(number);
+                    }
+                }
+            }
+
+           var readFile = File.ReadAllLinesAsync(filePath);
+           var fizzbuzz = new Fizzbuzz();
+           var result = string.Empty;
+
+           foreach (var line in readFile.Result)
+           {
+               result = fizzbuzz.Say(Convert.ToInt32(line));
+               System.Console.WriteLine($"{line} = {result}");
+           }
         }
     }
 }
